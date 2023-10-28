8c3b1c509a46634eaf366b6b224a20810859ecb4
rebibabo <2841329980@qq.com>
2023-10-28 01:00:48 +0000

    change class name from codemarker to scts

--- change_program_style.py
+++ change_program_style.py
@@ -10,7 +10,7 @@ except:
     os.system('pip install graphviz')
     from graphviz import Digraph
 
-class CodeMarker:
+class SCTS:
     def __init__(self, language):
         self.language = language
         if not os.path.exists(f'./build/{language}-languages.so'):
@@ -197,18 +197,18 @@ class CodeMarker:
         return tokens
 
 if __name__ == '__main__':
-    codemarker = CodeMarker('c')
+    scts = SCTS('c')
     code = open('test.c').read()
     print(code)
-    codemarker.see_tree(code)
-    print(codemarker.tokenize(code))
-    # new_code, succ = codemarker.change_file_style([8.11], code)
+    scts.see_tree(code)
+    print(scts.tokenize(code))
+    # new_code, succ = scts.change_file_style([8.11], code)
     # if succ:
     #     print(new_code)
     # for style_choice in [8.2, 8.3, 9.1, 9.2, 9.3, 10.1, 10.2, 10.3, 10.4]:
     #     print('style_choice:',style_choice)
-    #     codemarker.change_dir_style([style_choice], 'dataset/gcjpy_format', f'change/{style_choice}')
-    # codemarker.get_dir_popularity([5.1], 'dataset/ProgramData/2')
+    #     scts.change_dir_style([style_choice], 'dataset/gcjpy_format', f'change/{style_choice}')
+    # scts.get_dir_popularity([5.1], 'dataset/ProgramData/2')
 
 #[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.1, 1.2, 1.3, 1.4, 2.1, 2.2, 2.3, 2.4, 3.1, 3.2, 3.3, 3.4, 4.1, 4.2, 4.3, 4.4, 4.5, 5.1, 5.2, 6.1, 6.2, 6.3, 6.4, 6.5, 7.1, 7.2, 7.3, 7.4, 7.5, 8.1, 8.2, 8.3, 9.1, 9.2, 9.3, 10.1, 10.2, 10.3, 10.4]
 #[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.1, 1.2, 1.3, 1.4, 2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2, 3.3, 3.4, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 5.1, 5.2, 5.3, 5.4, 6.1, 6.2, 6.3, 6.4, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 8.11]
